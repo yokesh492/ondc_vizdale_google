1acfc9887ecb73a42641519cde5a4f757a9bd6a9
yogesh k <63460812+yokesh492@users.noreply.github.com>
2024-02-10 15:46:32 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.api.routes import router as api_router
 from app.api.auth import router
 from fastapi.middleware.cors import CORSMiddleware
+import os 
 
 app = FastAPI()
 
@@ -18,4 +19,5 @@ app.include_router(router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    port = int(os.getenv("PORT", "8000"))
+    uvicorn.run(app, host="0.0.0.0", port=port)
