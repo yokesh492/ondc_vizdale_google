cd97b11f974accb561d23ec7682998bf0f4c4391
yogesh k <63460812+yokesh492@users.noreply.github.com>
2024-02-10 20:25:40 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -19,5 +19,5 @@ app.include_router(router)
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT",8080))
+    port = int(os.enivron.get("PORT",8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
